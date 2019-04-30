b877acd795ed86b05091b9053ccac95d108df8d2
azure <rz7@outlook.jp>
2019-04-30 11:39:34 +0900

    OreDictCache with Java Streams

--- src/main/java/mekanism/common/OreDictCache.java
+++ src/main/java/mekanism/common/OreDictCache.java
@@ -1,8 +1,16 @@
 package mekanism.common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
 import mekanism.api.util.ItemInfo;
 import mekanism.common.util.ItemRegistryUtils;
 import net.minecraft.item.ItemBlock;
@@ -11,115 +19,64 @@ import net.minecraftforge.oredict.OreDictionary;
 
 public final class OreDictCache {
 
-    public static HashMap<ItemInfo, List<String>> cachedKeys = new HashMap<>();
-    public static HashMap<String, List<ItemStack>> oreDictStacks = new HashMap<>();
-    public static HashMap<String, List<ItemStack>> modIDStacks = new HashMap<>();
+    public static final HashMap<ItemInfo, List<String>> cachedKeys = new HashMap<>();
+    public static final HashMap<String, List<ItemStack>> oreDictStacks = new HashMap<>();
+    public static final HashMap<String, List<ItemStack>> modIDStacks = new HashMap<>();
 
-    public static List<String> getOreDictName(ItemStack check) {
-        if (check.isEmpty()) {
-            return new ArrayList<>();
-        }
+    public static List<String> getOreDictName(@Nonnull ItemStack check) {
+        if (check.isEmpty())
+            return new ArrayList<>(0);
 
-        ItemInfo info = ItemInfo.get(check);
-        List<String> cached = cachedKeys.get(info);
-
-        if (cached != null) {
-            return cached;
-        }
-
-        int[] idsFound = OreDictionary.getOreIDs(check);
-
-        List<String> ret = new ArrayList<>();
-
-        for (Integer id : idsFound) {
-            ret.add(OreDictionary.getOreName(id));
-        }
-
-        cachedKeys.put(info, ret);
-
-        return ret;
+        return cachedKeys.computeIfAbsent(ItemInfo.get(check),
+                k -> {
+                    return Arrays.stream(OreDictionary.getOreIDs(check))
+                            .mapToObj(OreDictionary::getOreName)
+                            .collect(Collectors.toCollection(ArrayList::new));
+                });
     }
 
     public static List<ItemStack> getOreDictStacks(String oreName, boolean forceBlock) {
-        if (oreDictStacks.get(oreName) != null) {
-            return oreDictStacks.get(oreName);
-        }
+        final Predicate<String> case1 = s -> oreName.equals(s) || oreName.equals("*");
+        final Predicate<String> case2 = s -> oreName.endsWith("*") && !oreName.startsWith("*")
+                && s.startsWith(oreName.substring(0, oreName.length() - 1));
+        final Predicate<String> case3 = s -> oreName.startsWith("*") && !oreName.endsWith("*")
+                && s.endsWith(oreName.substring(1));
+        final Predicate<String> case4 = s -> oreName.startsWith("*") && oreName.endsWith("*")
+                && s.contains(oreName.substring(1, oreName.length() - 1));
+        final Predicate<String> checks = case1.or(case2).or(case3).or(case4);
 
-        List<String> keys = new ArrayList<>();
+        return oreDictStacks.computeIfAbsent(oreName, k -> {
 
-        for (String s : OreDictionary.getOreNames()) {
-            if (s == null) {
-                continue;
-            }
-
-            if (oreName.equals(s) || oreName.equals("*")) {
-                keys.add(s);
-            } else if (oreName.endsWith("*") && !oreName.startsWith("*")) {
-                if (s.startsWith(oreName.substring(0, oreName.length() - 1))) {
-                    keys.add(s);
-                }
-            } else if (oreName.startsWith("*") && !oreName.endsWith("*")) {
-                if (s.endsWith(oreName.substring(1))) {
-                    keys.add(s);
-                }
-            } else if (oreName.startsWith("*") && oreName.endsWith("*")) {
-                if (s.contains(oreName.substring(1, oreName.length() - 1))) {
-                    keys.add(s);
-                }
-            }
-        }
-
-        List<ItemStack> stacks = new ArrayList<>();
-
-        for (String key : keys) {
-            for (ItemStack stack : OreDictionary.getOres(key)) {
-                ItemStack toAdd = stack.copy();
-
-                if (!stacks.contains(stack) && (!forceBlock || toAdd.getItem() instanceof ItemBlock)) {
-                    stacks.add(stack.copy());
-                }
-            }
-        }
-
-        oreDictStacks.put(oreName, stacks);
-
-        return stacks;
+            return Arrays
+                    .stream(OreDictionary.getOreNames())
+                    .filter(Objects::nonNull)
+                    .filter(checks)
+                    .map(OreDictionary::getOres)
+                    .flatMap(Collection::stream)
+                    .distinct()
+                    .filter(stack -> !forceBlock || stack.copy().getItem() instanceof ItemBlock)
+                    .collect(Collectors.toCollection(ArrayList::new));
+        });
     }
 
     public static List<ItemStack> getModIDStacks(String modName, boolean forceBlock) {
-        if (modIDStacks.get(modName) != null) {
-            return modIDStacks.get(modName);
-        }
+        Predicate<String> case1 = s -> (modName.equals(s) || modName.equals("*"));
+        Predicate<String> case2 = s -> (modName.endsWith("*") && !modName.startsWith("*")
+                && s.startsWith(modName.substring(0, modName.length() - 1)));
+        Predicate<String> case3 = s -> (modName.startsWith("*") && !modName.endsWith("*")
+                && s.endsWith(modName.substring(1)));
+        Predicate<String> case4 = s -> (modName.startsWith("*") && modName.endsWith("*")
+                && s.contains(modName.substring(1, modName.length() - 1)));
+        Predicate<String> checks = case1.or(case2).or(case3).or(case4);
 
-        List<ItemStack> stacks = new ArrayList<>();
-
-        for (String key : OreDictionary.getOreNames()) {
-            for (ItemStack stack : OreDictionary.getOres(key)) {
-                ItemStack toAdd = stack.copy();
-                String s = ItemRegistryUtils.getMod(toAdd);
-
-                if (!stacks.contains(stack) && toAdd.getItem() instanceof ItemBlock) {
-                    if (modName.equals(s) || modName.equals("*")) {
-                        stacks.add(stack.copy());
-                    } else if (modName.endsWith("*") && !modName.startsWith("*")) {
-                        if (s.startsWith(modName.substring(0, modName.length() - 1))) {
-                            stacks.add(stack.copy());
-                        }
-                    } else if (modName.startsWith("*") && !modName.endsWith("*")) {
-                        if (s.endsWith(modName.substring(1))) {
-                            stacks.add(stack.copy());
-                        }
-                    } else if (modName.startsWith("*") && modName.endsWith("*")) {
-                        if (s.contains(modName.substring(1, modName.length() - 1))) {
-                            stacks.add(stack.copy());
-                        }
-                    }
-                }
-            }
-        }
-
-        modIDStacks.put(modName, stacks);
-
-        return stacks;
+        return modIDStacks.computeIfAbsent(modName, k -> {
+            return Arrays.stream(OreDictionary.getOreNames())
+                    .map(OreDictionary::getOres)
+                    .flatMap(Collection::stream)
+                    .distinct()
+                    .filter(stack -> stack.getItem() instanceof ItemBlock)
+                    .filter(stack -> checks.test(ItemRegistryUtils.getMod(stack.copy())))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        });
     }
 }
