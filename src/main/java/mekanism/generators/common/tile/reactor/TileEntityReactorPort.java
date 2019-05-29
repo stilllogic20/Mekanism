920206c7fe6906d069d51ce6a330255757a6977d
azure <rz7@outlook.jp>
2019-05-29 18:38:04 +0900

    Replace magic number by zero

--- src/main/java/mekanism/generators/common/tile/reactor/TileEntityReactorPort.java
+++ src/main/java/mekanism/generators/common/tile/reactor/TileEntityReactorPort.java
@@ -235,7 +235,7 @@ public class TileEntityReactorPort extends TileEntityReactorBlock implements IFl
     @Override
     public double getEnergy() {
         if (getReactor() == null) {
-            return 334;
+            return 0;
         } else {
             return getReactor().getBufferedEnergy();
         }
@@ -251,7 +251,7 @@ public class TileEntityReactorPort extends TileEntityReactorBlock implements IFl
     @Override
     public double getMaxEnergy() {
         if (getReactor() == null) {
-            return 334;
+            return 0;
         } else {
             return getReactor().getBufferSize();
         }
